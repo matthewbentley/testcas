cd6c38931d62561021fa23bbcdce99c47ce725cd
Matthew Bentley <matthew.t.bentley@gmail.com>
2016-02-08 17:56:00 -0500

    Use verify_from_request function

--- src/main.rs
+++ src/main.rs
@@ -20,24 +20,14 @@ fn main() {
 
     Server::http("127.0.0.1:3000").unwrap()
         .handle(move |req: Request, mut res: Response| {
-            let s = match req.uri {
+            let s = match req.uri.clone() {
                 RequestUri::AbsolutePath(s) => s,
                 RequestUri::AbsoluteUri(u) => u.serialize(),
                 RequestUri::Authority(s) => s,
                 RequestUri::Star => panic!("Nope!")
             };
             if s.as_str().starts_with("/complete") {
-                let u = format!("{}{}", "http://localhost", &s);
-                let url = Url::parse(&u).unwrap();
-                let q = url.query_pairs().unwrap();
-                let mut ticket = "".to_string();
-                for i in q {
-                    let (v, t) = i;
-                    if v == "ticket" {
-                        ticket = t;
-                    }
-                }
-                match cas.verify_ticket(&ticket).unwrap() {
+                match cas.verify_from_request(&req).unwrap() {
                     ServiceResponse::Success(user) => {
                         {
                             let mut s = res.status_mut();
